1b216076f858ea10628f17d87fc768556474d885
gliefsoe <gert.liefsoens@telenet.be>
2022-12-29 13:58:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
     res.send('hello world from node sample 1: ' + req.url);
