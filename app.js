ab13f82eb604e10e9d40ea1da3ad50e91e474789
gliefsoe <gert.liefsoens@telenet.be>
2022-12-29 14:13:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+const packageJson = require('./package.json');
 const express = require('express');
 const app = express();
 const port = 8080;
+const version = packageJson.version;
 
 app.get('/', (req, res) => {
     res.send('hello world from node sample 1: ' + req.url);
 });
 
 app.get('/about', (req, res) => {
-    console.log(`Root of server: ${__dirname}`);
+    console.log('version:' + version);
     res.send('about')
   })
+
+
+console.log(`Root of server: ${__dirname}`);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
