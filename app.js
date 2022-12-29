85fd6b4d434ffc243d1e983e49c47c9f52a836af
gliefsoe <gert.liefsoens@telenet.be>
2022-12-29 13:21:40 +0100

    updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('hello world from node sample 1');
+    res.send('hello world from node sample 1: ' + req.url);
 });
 
 app.listen(port, () => {
